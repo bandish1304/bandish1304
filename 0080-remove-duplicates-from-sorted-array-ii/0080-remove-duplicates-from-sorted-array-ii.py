a779fdd6739cc4d1ab88c92c06043878806f35ad
Bandish Patel <151697802+bandish1304@users.noreply.github.com>
2025-04-07 11:22:45 -0700

    Time: 85 ms (60.82%), Space: 20.7 MB (20.22%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -9,9 +9,10 @@ class Solution:
                 count = count + 1
             else:
                 count = 1
-        
+                
             if count <= 2:
                 nums[j] = nums[i]
-                j = j + 1
-
+                j += 1
+                
         return j
+        
