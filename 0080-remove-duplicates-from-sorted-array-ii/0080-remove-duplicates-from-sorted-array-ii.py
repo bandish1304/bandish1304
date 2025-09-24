cd0e67c798e5ec412b9f9e0ec6ae65d8555751c3
Bandish Patel <151697802+bandish1304@users.noreply.github.com>
2025-09-24 09:47:56 -0700

    Time: 94 ms (16.75%), Space: 20.7 MB (18.11%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -6,13 +6,11 @@ class Solution:
 
         for i in range(1, n):
             if nums[i] == nums[i-1]:
-                count = count + 1
+                count += 1
             else:
                 count = 1
-                
+
             if count <= 2:
                 nums[j] = nums[i]
                 j += 1
-                
         return j
-        
