c4d0cb4173317c29f14eecc6e7f3676684b03a59
Bandish Patel <151697802+bandish1304@users.noreply.github.com>
2025-10-18 07:33:02 -0700

    Time: 0 ms (100%), Space: 18.1 MB (40.55%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,16 +1,18 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        counts = [0] *(len(citations) + 1)
-
+        # Make a count array
+        count = [0] * (len(citations) + 1)
+        # Then do a for loop
         for item in citations:
-            if item >= len(citations):
-                counts[len(citations)] += 1
-            else: 
-                counts[item] += 1
-
+            if item > len(citations):
+                count[len(citations)] += 1
+            else:
+                count[item] += 1
+        # Now go backward loop
         total_citations = 0
-        for i in range(len(counts) -1, -1, -1):
-            total_citations += counts[i]
+        for i in range(len(count)-1, -1, -1):
+            total_citations += count[i]
             if total_citations >= i:
                 return i
         return 0
+        
