d619f0c683ebacd9636b0e3f6d9673083c295522
Bandish Patel <151697802+bandish1304@users.noreply.github.com>
2026-02-11 19:21:44 -0800

    Time: 17 ms (33.36%), Space: 24.2 MB (8.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
+        res = defaultdict(list)  # This creates a key with empty list
+
+        # Now iterate through input strings and 
+        # make all of them 0
         for s in strs:
             count = [0] * 26
+
+        # Now find each letter index values
             for c in s:
                 count[ord(c) - ord('a')] += 1
+
+        # and then append
             res[tuple(count)].append(s)
         return list(res.values())
