9759641cdef603e974384435eca0cfff015aa6de
Bandish Patel <151697802+bandish1304@users.noreply.github.com>
2026-06-27 09:13:15 -0700

    Time: 0 ms (100%), Space: 19.3 MB (77.72%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,12 @@
+# defaultdict set all numbers default to 0
+from collections import defaultdict
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        counts = defaultdict(int)
+        for nums in arr:
+            counts[nums] += 1
+        # Now we have to count the total(values) of each numbers
+        occur = list(counts.values())
+        # check if len(occur) is equal to set then return true
+        # otherwise return false
+        return len(occur) == len(set(occur))
