d05939293d5ecdde9295d1afa0a62b6200ff59df
Bandish Patel <151697802+bandish1304@users.noreply.github.com>
2026-07-17 12:00:30 -0700

    Time: 0 ms (100%), Space: 19.2 MB (64.64%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,5 +1,6 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+    #First we create two pointers i and j
         i = 0
         j = 0
 
@@ -7,7 +8,7 @@ class Solution:
             if s[i] == t[j]:
                 i += 1
             j += 1
-
+    # Now check if i is at the last character then its true or its false
         if i == len(s):
             return True
         else:
