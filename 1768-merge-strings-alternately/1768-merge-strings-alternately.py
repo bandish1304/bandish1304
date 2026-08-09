f60a6b4be968a0ffd6eb487f18264d4b88787701
Bandish Patel <151697802+bandish1304@users.noreply.github.com>
2026-08-09 12:40:08 -0700

    Time: 43 ms (71.41%), Space: 19.2 MB (56.68%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -4,16 +4,14 @@ class Solution:
         i = 0
         j = 0
         merge = ""
-        len_w1 = len(word1)
-        len_w2 = len(word2)
         
         # Now do a while loop and make a merge
-        while i < len_w1 and j < len_w2:
+        while i < len(word1) and j < len(word2):
             merge += word1[i] + word2[j]
             i += 1
             j += 1
         # And then check is one word is greather than other
-        if i < len_w1:
+        if i < len(word1):
             merge += word1[i:]
         else:
             merge += word2[j:]
